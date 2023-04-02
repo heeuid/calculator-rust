b3369a003fa39e71cca70296a7fed19c72b18b01
Sunghee Kim <h33uid@gmail.com>
2023-04-02 23:47:59 +0900

    Update open_dir prjt: to change directory

--- open_dir/src/main.rs
+++ open_dir/src/main.rs
@@ -1,3 +1,11 @@
+enum FileType {
+    File,
+    Directory,
+    SymbolicFile,
+    SymbolicDirectory,
+    Other,
+}
+
 enum EventHandle {
     Quit,
     Move,
@@ -5,11 +13,11 @@ enum EventHandle {
 }
 
 struct App {
-    contents: Vec<String>,
+    contents: Vec<(String, FileType)>,
     curr_location: String,
     curr_line: u16,
     view_line_start: u16,
-    //information: Vec<String>,
+    //information: Vec<String>, // TODO
 }
 
 impl App {
@@ -26,17 +34,20 @@ impl App {
             ),
             curr_line: 0, // line (index) of contents list
             view_line_start: 0,
+            //information: Vec<String>, // TODO
         }
     }
 
-    fn create_list_by_location(location: &String) -> Result<Vec<String>, std::io::Error> {
+    fn create_list_by_location(
+        location: &String,
+    ) -> Result<Vec<(String, FileType)>, std::io::Error> {
         // 1) get list(files, dirs) in this location
         let mut paths = std::fs::read_dir(location)?;
 
         // 2) vector push: all stuffs in this location
-        let mut contents_vec = Vec::<String>::new();
+        let mut contents_vec = Vec::<(String, FileType)>::new();
         if std::fs::canonicalize(location).unwrap().to_str().unwrap() != "/" {
-            contents_vec.push(String::from(".."));
+            contents_vec.push((String::from(".."), FileType::Directory));
         }
         for path in &mut paths {
             let rel_path = path.unwrap().path();
@@ -45,6 +56,7 @@ impl App {
             let symlink_meta = std::fs::symlink_metadata(&rel_path)?;
 
             let mut displable_name = String::from(file_name);
+            let mut file_type = FileType::File;
 
             if symlink_meta.file_type().is_symlink() {
                 let real_path = std::fs::read_link(&rel_path).unwrap();
@@ -53,15 +65,22 @@ impl App {
                     rel_path.file_name().unwrap().to_str().unwrap(),
                     real_path.to_str().unwrap(),
                 );
+                file_type = FileType::SymbolicFile;
             }
 
             if abs_path.is_dir() {
                 displable_name += "/";
+                if let FileType::SymbolicFile = file_type {
+                    file_type = FileType::SymbolicDirectory;
+                } else {
+                    file_type = FileType::Directory;
+                }
             } else if !abs_path.is_file() {
                 displable_name += "?";
+                file_type = FileType::Other;
             }
 
-            contents_vec.push(displable_name);
+            contents_vec.push((displable_name, file_type));
         }
 
         Ok(contents_vec)
@@ -73,9 +92,42 @@ impl App {
 
         // 2) sort vector
         let slice = &mut self.contents[2..];
-        slice.sort();
+        slice.sort_by(|a, b| a.0.cmp(&b.0));
 
         Ok(())
+    }
+
+    fn change_directory(&mut self) -> Result<bool, std::io::Error> {
+        let next_content = &self.contents[self.curr_line as usize];
+        let mut path = String::new();
+        let ret = match next_content.1 {
+            FileType::Directory => {
+                path = format!("{}/{}", &self.curr_location, &next_content.0);
+                Ok(true)
+            }
+            FileType::SymbolicDirectory => {
+                path = format!(
+                    "{}/{}",
+                    &self.curr_location,
+                    next_content
+                        .0
+                        .split_whitespace()
+                        .next()
+                        .unwrap()
+                        .to_string()
+                );
+                Ok(true)
+            }
+            _ => Ok(false),
+        };
+
+        if let Ok(true) = ret {
+            let abs_path = std::fs::canonicalize(std::path::Path::new(&path))?;
+            self.curr_location = String::from(abs_path.to_str().unwrap());
+            self.curr_line = 0;
+            self.view_line_start = 0;
+        }
+        ret
     }
 }
 
@@ -107,24 +159,6 @@ fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn handle_arguments(app: &mut App, args: &Vec<String>) {
-    let mut i = 0;
-    let length = args.len();
-    while i < length {
-        let arg = &args[i];
-        if arg == "--path" && i + 1 < length {
-            i += 1;
-            let path = &args[i];
-            if let Ok(_) = std::fs::metadata(path) {
-                app.curr_location =
-                    String::from(std::fs::canonicalize(path).unwrap().to_str().unwrap());
-            }
-        }
-
-        i += 1;
-    }
-}
-
 fn run_app<B: tui::backend::Backend>(
     terminal: &mut tui::terminal::Terminal<B>,
     app: &mut App,
@@ -150,6 +184,24 @@ fn run_app<B: tui::backend::Backend>(
     }
 
     Ok(())
+}
+
+fn handle_arguments(app: &mut App, args: &Vec<String>) {
+    let mut i = 0;
+    let length = args.len();
+    while i < length {
+        let arg = &args[i];
+        if arg == "--path" && i + 1 < length {
+            i += 1;
+            let path = &args[i];
+            if let Ok(_) = std::fs::metadata(path) {
+                app.curr_location =
+                    String::from(std::fs::canonicalize(path).unwrap().to_str().unwrap());
+            }
+        }
+
+        i += 1;
+    }
 }
 
 fn handle_event(
@@ -179,6 +231,14 @@ fn handle_event(
                         app.curr_line -= 1;
                     }
                     return Ok(EventHandle::Move);
+                }
+                crossterm::event::KeyCode::Enter => {
+                    if app.change_directory()? {
+                        // 1) renew contents list with changed current location
+                        app.contents = App::create_list_by_location(&app.curr_location)?;
+
+                        // 2) display information: // TODO
+                    }
                 }
                 _ => {}
             },
@@ -226,7 +286,7 @@ fn ui<B: tui::backend::Backend>(f: &mut tui::terminal::Frame<B>, app: &mut App) 
     // 4) render paragraph for contents
 
     // calculate begining view index of contents list vector
-    let view_height = chunks[0].height - 2;
+    let view_height = chunks[0].height - 2; //2: border block size
     if app.view_line_start + view_height <= app.curr_line {
         app.view_line_start = app.curr_line - view_height + 1;
     } else if app.view_line_start > app.curr_line {
@@ -244,7 +304,9 @@ fn ui<B: tui::backend::Backend>(f: &mut tui::terminal::Frame<B>, app: &mut App) 
     // create view contents vector
     let mut i = 0 as usize;
     let mut text: Vec<tui::text::Spans> = vec![];
-    for path in &app.contents[start_i..end_i] {
+    for content in &app.contents[start_i..end_i] {
+        let path = &content.0;
+        //let file_type = &content.1;
         if i == curr_i - start_i as usize {
             text.push(tui::text::Spans::from(vec![tui::text::Span::styled(
                 path.as_str(),
@@ -253,8 +315,14 @@ fn ui<B: tui::backend::Backend>(f: &mut tui::terminal::Frame<B>, app: &mut App) 
                     .fg(tui::style::Color::Black),
             )]));
         } else {
-            text.push(tui::text::Spans::from(path.as_str()));
+            text.push(tui::text::Spans::from(vec![tui::text::Span::styled(
+                path.as_str(),
+                tui::style::Style::default()
+                    .bg(tui::style::Color::Black)
+                    .fg(tui::style::Color::White),
+            )]));
         }
+
         i += 1;
     }
 
